d9e51dd162a9b4ef072512a31a594e68979bfb15
JavvadiDhanush <dhanushjavvadi@gmail.com>
2025-07-26 11:34:08 +0530

    Add files via upload

---
+++ OOPS9.java
@@ -0,0 +1,13 @@
+package DHANUSH;
+
+public class OOPS9 {
+    public static void main(String args[]) {
+ 
+        Integer a = new Integer(3);
+
+        int i = a.intValue(); 
+        int j = a;            
+
+        System.out.println(a + " " + i + " " + j);
+    }
+}
