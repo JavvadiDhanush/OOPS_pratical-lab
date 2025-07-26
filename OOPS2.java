13e93fe89bc23d4ba5eddcbabffdf3aea06020fb
JavvadiDhanush <dhanushjavvadi@gmail.com>
2025-07-26 11:31:36 +0530

    Add files via upload

---
+++ OOPS2.java
@@ -0,0 +1,20 @@
+package DHANUSH;
+
+
+import java.io.*;
+
+public class OOPS2 {
+ public static void main(String[] args) {
+     int i = 10;
+
+    
+     long l = i;
+
+     
+     double d = i;
+
+     System.out.println("Integer: " + i);
+     System.out.println("Long: " + l);
+     System.out.println("Double: " + d);
+ }
+}
